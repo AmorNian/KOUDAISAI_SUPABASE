fd79fb98d5385e1814375ae920cb1b9388d2e7c8
AmorNian <1226697369@qq.com>
2025-10-16 18:36:42 +0900

    add chechposition and admin login

--- app.py
+++ app.py
@@ -7,51 +7,69 @@ app = Flask(__name__)
 url = "https://ngfkaxoxnsukgntmrtur.supabase.co"
 key = "sb_secret_4MHsA0C2_W_MXEiCFyp5lQ_7l6i0uQO"
 supabase: Client = create_client(url, key)
+adminpassword = "Sakaguchi2025"
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+@app.route('/adminlogin')
+def adminlogin():
+    return render_template("adminlogin.html")
+
 @app.route("/get_number", methods=['POST'])
 def get_number():
     data = request.json
     name = data["name"]
+    # 没有输入名字
     if len(name) == 0:
         return {"status": 1}
-    # 获取当前最大号码
-    res = supabase.table("User").select("id").order("id", desc=True).limit(1).execute()
-    max_number = res.data[0]["id"] if res.data else 0
+    
+    res = supabase.table("User").select("*").eq("name", name).execute()
+    if res.data:
+        # 重名了
+        return {"status": 2}
+    else:
+        # 获取当前最大号码
+        res = supabase.table("User").select("id").order("id", desc=True).limit(1).execute()
+        max_number = res.data[0]["id"] if res.data else 0
 
-    new_number = max_number + 1
-    supabase.table("User").insert({"name": name, "id": new_number, "status": 0, "time_start":"10:10"}).execute()
-    return {"number": new_number, "status": 0}
+        new_number = max_number + 1
+        supabase.table("User").insert({"name": name, "id": new_number, "status": 0, "time_start":"10:10"}).execute()
+        return {"number": new_number, "status": 0}
 
 # ===== 查询前方人数接口 =====
-@app.route("/check_position")
-async def check_position(request: request):
-    data = await request.json()
+@app.route("/check_position", methods=['POST'])
+def check_position():
+    data = request.json
     name = data["name"]
 
     # 查找用户的号码
-    res = supabase.table("queue").select("number").eq("name", name).execute()
+    res = supabase.table("User").select("id").eq("name", name).execute()
     if not res.data:
-        return {"error": "User not found"}
+        return {"status": 1}
 
-    user_number = res.data[0]["number"]
+    user_number = res.data[0]["id"]
     # 查找前方还在等待的人
-    res2 = supabase.table("queue").select("number").lt("number", user_number).eq("status", "waiting").execute()
+    res2 = supabase.table("User").select("id").lt("id", user_number).eq("status", 0).execute()
     waiting_count = len(res2.data)
 
     return {"number": user_number, "waiting": waiting_count}
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Render 会提供 PORT 环境变量
-    app.run(host="0.0.0.0", port=port)
-'''
-@app.route('/')
-def hello_world():
-    return 'hello world'
+@app.route("/admin_login", methods=['POST'])
+def admin_login():
+    data = request.json
+    pwd = data["password"]
+    if pwd == adminpassword:
+        return {"status": 0}
+    else:
+        return {"status": 1}
 
-if __name__ == '__main__':
+""" if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Render 会提供 PORT 环境变量
+    app.run(host="0.0.0.0", port=port) """
+
+
+if __name__ == "__main__":
     app.run(debug=True)
-'''
+
